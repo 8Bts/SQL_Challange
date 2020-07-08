6a2fd8d3119714b656b86e3a028b2c2e3527527f
8Bts <mcmizze@yahoo.com>
2020-07-08 18:35:41 +0400

    Add solution queries for SELECT from Nobel challange

--- SELECT_from_World.sql
+++ SELECT_from_World.sql
@@ -72,4 +72,12 @@ SELECT name, capital FROM world WHERE LENGTH(name) = LENGTH(capital)
 */
 
 SELECT name, capital FROM world
-WHERE LEFT(name, 1) = LEFT(capital, 1) AND name <> capital
+ WHERE LEFT(name, 1) = LEFT(capital, 1) AND name <> capital
+
+/* 13.
+ Find the country that has all the vowels and no spaces in its name.
+ */
+
+ SELECT name FROM world WHERE name LIKE '%a%' AND name LIKE '%e%' AND
+  name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%'
+  AND name NOT LIKE '% %'
